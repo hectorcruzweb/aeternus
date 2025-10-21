8c8c1891685bce2e51469c761d690e628d852c5a
Héctor Cruz <hectorcrzprz@gmail.com>
2025-10-21 00:21:46 -0700

    reparado dropdown menu, abre y cierra on click y cierra en unfocus

--- resources/js/src/layouts/components/navbar/components/ProfileDropDown.vue
+++ resources/js/src/layouts/components/navbar/components/ProfileDropDown.vue
@@ -1,75 +1,27 @@
 <template>
-  <div
-    class="the-navbar__user-meta flex items-center"
-    v-if="this.activeUserInfo"
-  >
+  <div class="the-navbar__user-meta flex items-center" v-if="activeUserInfo">
+    <!-- User info -->
     <div class="text-right leading-tight hidden sm:block">
-      <p class="size-base font-bold lowercase">
-        {{ this.activeUserInfo.nombre }}
-      </p>
+      <p class="size-base font-bold lowercase">{{ activeUserInfo.nombre }}</p>
       <small class="size-smaller color-copy">Disponible</small>
     </div>
-    <vs-dropdown vs-custom-content vs-trigger-click:true class="cursor-pointer">
-      <div class="con-img ml-3" v-if="this.activeUserInfo.imagen">
-        <img
-          key="onlineImg"
-          :src="activeUserInfo.imagen"
-          alt="user-img"
-          width="40"
-          height="40"
-          class="rounded-full shadow-md cursor-pointer block"
-        />
-      </div>
-      <div class="con-img ml-3" v-else>
-        <img
-          key="onlineImg"
-          src="@assets/images/portrait/small/default-user.jpg"
-          alt="user-img"
-          width="40"
-          height="40"
-          class="rounded-full shadow-md cursor-pointer block"
-        />
+
+    <!-- Avatar and dropdown -->
+    <vs-dropdown vs-custom-content vs-trigger-click class="cursor-pointer" ref="userDropdown">
+      <!-- Avatar -->
+      <div class="con-img ml-3">
+        <img :src="activeUserInfo.imagen || require('@assets/images/portrait/small/default-user.jpg')" alt="user-img"
+          width="40" height="40" class="rounded-full shadow-md cursor-pointer block" />
       </div>
 
+      <!-- Dropdown Menu -->
       <vs-dropdown-menu class="vx-navbar-dropdown menu-dropdown-aeternus">
         <ul style="min-width: 9rem">
-          <li
-            class="flex py-2 px-4"
-            @click="$router.push('/pages/profile').catch(() => {})"
-          >
+          <li class="flex py-2 px-4" @click="goToProfile">
             <feather-icon icon="UserIcon" svgClasses="w-4 h-4" />
             <span class="ml-2">Perfil</span>
           </li>
-          <!--
-          <li
-            class="flex py-2 px-4"
-            @click="$router.push('/home').catch(() => {})"
-          >
-            <feather-icon icon="MailIcon" svgClasses="w-4 h-4" />
-            <span class="ml-2">Soporte</span>
-          </li>
-          
-          <li
-            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-            @click="$router.push('/apps/todo').catch(() => {})">
-            <feather-icon icon="CheckSquareIcon" svgClasses="w-4 h-4" />
-            <span class="ml-2">Tasks</span>
-          </li>
 
-          <li
-            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-            @click="$router.push('/apps/chat').catch(() => {})">
-            <feather-icon icon="MessageSquareIcon" svgClasses="w-4 h-4" />
-            <span class="ml-2">Chat</span>
-          </li>
-
-          <li
-            class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-            @click="$router.push('/apps/eCommerce/wish-list').catch(() => {})">
-            <feather-icon icon="HeartIcon" svgClasses="w-4 h-4" />
-            <span class="ml-2">Wish List</span>
-          </li>
-          -->
           <vs-divider class="m-1" />
 
           <li class="flex py-2 px-4" @click="openConfirmarSinPassword = true">
@@ -79,18 +31,17 @@
         </ul>
       </vs-dropdown-menu>
     </vs-dropdown>
-    <ConfirmarDanger
-      :show="openConfirmarSinPassword"
-      :callback-on-success="logout"
-      @closeVerificar="openConfirmarSinPassword = false"
-      :accion="'Esta operación lo sacará de sistema.'"
-      :confirmarButton="'Confirmar y salir'"
-    ></ConfirmarDanger>
+
+    <!-- Confirm logout -->
+    <ConfirmarDanger :show="openConfirmarSinPassword" :callback-on-success="logout"
+      @closeVerificar="openConfirmarSinPassword = false" :accion="'Esta operación lo sacará del sistema.'"
+      :confirmarButton="'Confirmar y salir'" />
   </div>
 </template>
 
 <script>
 import ConfirmarDanger from "@pages/ConfirmarDanger";
+
 export default {
   data() {
     return {
@@ -98,31 +49,52 @@ export default {
       openConfirmarSinPassword: false,
     };
   },
-  components: {
-    ConfirmarDanger,
-  },
+  components: { ConfirmarDanger },
   methods: {
+    closeDropdown() {
+      this.$nextTick(() => {
+        if (this.$refs.userDropdown) {
+          this.$refs.userDropdown.closeDropdown();
+        }
+      });
+    },
+    goToProfile() {
+      this.$router.push("/pages/profile").catch(() => { });
+      this.closeDropdown();
+    },
     logout() {
       this.$store.dispatch("auth/logout_user");
     },
-  },
-  mounted() {
-    /**si no existe mando llamar los datos del usuario get_perfil de rutas api */
-    if (!localStorage.getItem("userInfo")) {
-      this.$store.dispatch("auth/user_datos").then((resp) => {
-        this.activeUserInfo = JSON.parse(localStorage.getItem("userInfo"));
-      });
-    } else {
-      this.activeUserInfo = JSON.parse(localStorage.getItem("userInfo"));
-      if (!this.activeUserInfo.user_id) {
-        this.$store.dispatch("auth/user_datos").then((resp) => {
-          this.activeUserInfo = JSON.parse(localStorage.getItem("userInfo"));
-        });
+    handleClickOutside(event) {
+      const dropdownRef = this.$refs.userDropdown;
+      if (dropdownRef && dropdownRef.$el && !dropdownRef.$el.contains(event.target)) {
+        this.closeDropdown();
       }
     }
+
   },
-  created() {
-    this.activeUserInfo = JSON.parse(localStorage.getItem("userInfo"));
+  mounted() {
+    // Load user info
+    const user = localStorage.getItem("userInfo");
+    if (user) {
+      this.activeUserInfo = JSON.parse(user);
+    } else {
+      this.$store.dispatch("auth/user_datos").then(() => {
+        this.activeUserInfo = JSON.parse(localStorage.getItem("userInfo"));
+      });
+    }
+
+    // Close dropdown if clicking outside
+    document.addEventListener("click", this.handleClickOutside);
+
+    // Close dropdown on **any route change**
+    this.unwatch = this.$router.afterEach(() => {
+      this.closeDropdown();
+    });
+  },
+  beforeDestroy() {
+    document.removeEventListener("click", this.handleClickOutside);
+    if (this.unwatch) this.unwatch(); // remove afterEach listener
   },
 };
 </script>
