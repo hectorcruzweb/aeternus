4b81b28e22e2a8822a8779505746259c9a3339cc
Héctor Cruz <hectorcrzprz@gmail.com>
2025-11-19 18:09:30 -0700

    protegida ruta de reporte de adeudos_cfdis

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@
  */
 
 /**en pruebas */
-Route::get('adeudos_cfdis', 'ReportesController@adeudos_cfdis');
+
 /**ruta para obtener tokens */
 Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
 /**rutas de modulo en proceso */
@@ -289,6 +289,7 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('facturacion/cancelar_cfdi_folio', 'FacturacionController@cancelar_cfdi_folio')->middleware(['permiso:21,61']);
     /**rutas de reportes */
     Route::get('reportes/get_reportes', 'ReportesController@get_reportes');
+    Route::get('adeudos_cfdis', 'ReportesController@adeudos_cfdis');
     /**rutas de checador via web */
     Route::post('/checador/modificar_registro_administrativo', 'ChecadorController@modificar_registro_administrativo')->middleware(['permiso:23,66']);
     Route::post('/checador/guardar_registro_administrativo', 'ChecadorController@guardar_registro_administrativo')->middleware(['permiso:23,65']);
